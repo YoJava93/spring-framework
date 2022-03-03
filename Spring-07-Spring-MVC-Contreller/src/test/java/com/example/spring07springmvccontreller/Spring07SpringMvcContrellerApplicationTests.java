c59ae8ccee4c4e7b0d68f3fcdbd5b8dc934e4ed8
alexandrcorenciuc <alexandrcorenciuc@gmail.com>
2022-03-02 22:10:59 -0800

    Spring MVC

---
+++ Spring-07-Spring-MVC-Contreller/src/test/java/com/example/spring07springmvccontreller/Spring07SpringMvcContrellerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring07springmvccontreller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring07SpringMvcContrellerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
